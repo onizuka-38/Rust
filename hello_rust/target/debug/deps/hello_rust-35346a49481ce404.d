922c6b3632afd2f51c50a62b1f908f87fe6d8429
onizuka-38 <bdh9938@gmail.com>
2025-10-21 11:42:36 +0900

    rust

---
+++ hello_rust/target/debug/deps/hello_rust-35346a49481ce404.d
@@ -0,0 +1,5 @@
+c:\Rust\hello_rust\target\debug\deps\hello_rust-35346a49481ce404.d: src\main.rs
+
+c:\Rust\hello_rust\target\debug\deps\hello_rust-35346a49481ce404.exe: src\main.rs
+
+src\main.rs:
--- hello_rust/target/debug/deps/hello_rust.d
+++ hello_rust/target/debug/deps/hello_rust.d
@@ -1,5 +1,5 @@
-C:\Rust\hello_rust\target\debug\deps\hello_rust.d: src\main.rs
+c:\Rust\hello_rust\target\debug\deps\hello_rust.d: src\main.rs
 
-C:\Rust\hello_rust\target\debug\deps\hello_rust.exe: src\main.rs
+c:\Rust\hello_rust\target\debug\deps\hello_rust.exe: src\main.rs
 
 src\main.rs:
